b435104b4027548c9e5fe2618eece5101204c771
Amir Hossain <amir508_du@yahoo.com>
2022-04-17 06:25:58 +0600

    routes established and header component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Header from './shared/Header/Header'
+import Blog from './pages/Blog/Blog'
+import About from './pages/About/About'
+import Login from './pages/Login/Login'
+import NotFound from './pages/NotFound/NotFound'
 
 const App = () => {
   return (
-    <div>Hello React</div>
+    <>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/blog" element={<Blog/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </>
   )
 }
 
--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is header</h2>
+        <header className="text-gray-600 body-font bg-green-700">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <Link className = "flex title-font text-2xl font-medium items-center text-white mb-4 md:mb-0 cursor-pointer" to="/home" >
+            Nutrtion by Ameera
+            </Link>
+            <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+            <Link className = "mr-5 text-white text-xl  hover:text-yellow-200 cursor-pointer" to="/"> Home </Link>
+            <Link className = "mr-5 text-white text-xl hover:text-yellow-200 cursor-pointer" to="/blog"> Blog </Link>
+            <Link className = "mr-5 text-white text-xl hover:text-yellow-200 cursor-pointer" to="/about"> About Me </Link>
+            </nav>
+            <Link className = "inline-flex items-center text-xl bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 cursor-pointer" to="/login"> Login
+            </Link>
         </div>
+    </header>
     );
 };
 
