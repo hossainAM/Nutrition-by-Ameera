5662cf6359753f6a7421be133a8df0eaba33f9e4
Amir Hossain <amir508_du@yahoo.com>
2022-04-18 19:33:44 +0600

    google sign in redirection addedd

--- src/pages/Login/SocialLogin.js
+++ src/pages/Login/SocialLogin.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import Spinner from '../../shared/Spinner';
 
 const SocialLogin = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     const navigate = useNavigate();
+    const location = useLocation();
+
+    let from = location.state?.from?.pathname || "/";
 
     let errorElement;
 
@@ -18,9 +21,12 @@ const SocialLogin = () => {
       errorElement =
         <p>Error:{error?.message}</p>
   }
-    if (user) {
-       navigate('/home')
-    }
+
+		if(user) {
+		navigate(from, {
+			replace: true
+		});
+	}
 
     return (
         <>
